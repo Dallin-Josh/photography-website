fae296af608f201a16041ba71d1a2f89a8d4ba55
joshwilcken <joshwilcken@gmail.com>
2017-09-12 15:31:51 -0600

    committing changes to router and header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
-import About from './components/About/About';
-import Landing from './components/Landing/Landing';
+import Landing from './components/Landing/Landing'
 import Pricing from './components/Pricing/Pricing'
+import About from './components/About/About'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing}></Route>
+            <Route path='/about' component={About}></Route>
+            <Route path='/pricing' component={Pricing}></Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './Header.css'
+import {Link} from 'react-router-dom'
 
 export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                
+                <Link to='/'><div>Home</div></Link>
+                <Link to='/about'><div>About</div></Link>
+                <Link to='/pricing'><div>Pricing</div></Link>
             </div>
         )
     }
