87a15cfcd5b7f8ad92ce38db6b51095ac83a80fa
joshwilcken <joshwilcken@gmail.com>
2017-09-12 13:24:52 -0600

    committing started header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header'
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
+        <Header />
       </div>
     );
   }
