7d985ec7bd0f8166c8e498f9a824592ac7cbab0b
Dallin <andersondallin11@gmail.com>
2017-09-12 15:03:17 -0600

    Established Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header/Header';
 import About from './components/About/About';
 import Landing from './components/Landing/Landing';
 import Pricing from './components/Pricing/Pricing'
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+
+        <Landing />
       </div>
     );
   }
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import './Landing.css'
+
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+            <Header />
+            <div className="family-landing-pic" >
+            
+            </div>
+            <Footer />
+            </div>
+        );
+    }
+}
+
+export default Landing;
