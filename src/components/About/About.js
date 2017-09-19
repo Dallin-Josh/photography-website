5b308855c6ed52cd94e249311c20414adbabe643
joshwilcken <joshwilcken@gmail.com>
2017-09-18 19:23:47 -0600

    changed the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header'
 import './About.css'
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                Welcome to the About Page!
+                <Header />
+                <div className='about-page'>
+                    <img />
+                    <div>About</div>
+                </div>
             </div>
         )
     }
