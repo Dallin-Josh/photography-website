45550eccaa9d51746720d80646356f26337044fa
joshwilcken <joshwilcken@gmail.com>
2017-09-19 20:05:03 -0600

    added link to pinterest

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -14,7 +14,7 @@ class Landing extends Component {
                     <a href=''><i className="fa fa-facebook-official fa-2x" aria-hidden="true" ></i></a>
                     <a href=''><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
                     <a href=''><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-                    <a href=''><i class="fa fa-pinterest-square fa-2x" aria-hidden="true"></i></a>
+                    <a href=''><i className="fa fa-pinterest-square fa-2x" aria-hidden="true"></i></a>
                 </div>
             </div>
             </div>
